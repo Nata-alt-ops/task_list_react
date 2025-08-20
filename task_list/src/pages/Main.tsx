d394ba9d2f94003ec76f8dc764987b6501b136ab
n <nataliaklucnikova9@gmail.com>
2025-08-20 10:22:52 +0300

    \

--- task_list/src/pages/Main.tsx
+++ task_list/src/pages/Main.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import './Main.scss';
+
+type Task = {
+    id:number;
+    task:string;
+}
 
 export const Main = () =>{
+    const [problems, setProblem] = useState<Task[]>(
+        [
+            {id:1,
+             task:"Помыть кота"
+            }
+        ]
+    )
     return(
-        <p>привет</p>
+        <div className="site_con">
+            <div className="list_con">
+                <div className="list">
+                    <h1 className="list_title">Список дел</h1>
+                    {problems.map(problem =>(
+                        <div key={problem.id}>
+                            <div className="list_task">{problem.task}</div>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+        </div>
 
     );
 }
