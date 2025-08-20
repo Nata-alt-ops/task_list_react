895782c34cdabbc127eef525345c491568f6282d
n <nataliaklucnikova9@gmail.com>
2025-08-20 12:46:49 +0300

    \

--- task_list/src/pages/Main.tsx
+++ task_list/src/pages/Main.tsx
@@ -7,6 +7,7 @@ type Task = {
 }
 
 export const Main = () =>{
+    
     const [problems, setProblem] = useState<Task[]>(
         [
             {id:1,
@@ -14,6 +15,11 @@ export const Main = () =>{
             }
         ]
     )
+    const DeleteTask = (id:number) =>{
+       setProblem(nTask => nTask.filter(task => task.id !== id))
+    }
+
+   
     return(
         <div className="site_con">
             <div className="list_con">
@@ -24,13 +30,21 @@ export const Main = () =>{
                             <div className="list_task">
                                 <div className="task_text">{problem.task}</div>
                                 <div className="actions_icons">
-                                  
-                                    <img src="/pen.svg" alt={problem.task} className="change_task" />
-                                    <img src="/delete (1).png" alt={problem.task} className="delete_task" />
+                                    <img src="/edit (1).png" alt={problem.task} className="change_task" />
+                                    <img src="/delete (1).png" alt={problem.task} className="delete_task" 
+                                    onClick={() => DeleteTask(problem.id)}/>
                                 </div>
                             </div>
                         </div>
                     ))}
+
+                    <div className="new_task">
+                        <h1>Новая запись</h1>
+                        <div className="input_button">
+                            <input placeholder="Новая запись"></input>
+                            <button>Добавить</button>
+                        </div>
+                    </div>
 
                 </div>
             </div>
