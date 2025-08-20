ab0323e994a6a0f084d7e304ed0ccd508c759db4
n <nataliaklucnikova9@gmail.com>
2025-08-20 16:56:18 +0300

    \

--- task_list/src/pages/Main.tsx
+++ task_list/src/pages/Main.tsx
@@ -4,20 +4,67 @@ import './Main.scss';
 type Task = {
     id:number;
     task:string;
+    completed:boolean;
+
 }
+
+
 
 export const Main = () =>{
     
     const [problems, setProblem] = useState<Task[]>(
         [
             {id:1,
-             task:"Помыть кота"
+             task:"Помыть кота",
+             completed: false
+            },
+            {id:2,
+             task:"Почесать кота",
+             completed: false
             }
         ]
     )
     const DeleteTask = (id:number) =>{
        setProblem(nTask => nTask.filter(task => task.id !== id))
     }
+    const [newTask, setNewTask] = useState('');
+    const addTask = () => {
+        if (newTask.trim() === '') return;
+
+        const newtask: Task = {
+            id: problems.length > 0 ? Math.max(...problems.map(u => u.id))+1:1,
+            task:newTask.trim(),
+            completed:false
+        };
+        setProblem([...problems, newtask]);
+        setNewTask('')
+
+    };
+
+    const[editTaskId, setEditTaskId] = useState<number | null>(null);
+    const[editTask, setEditTask] = useState('');
+
+    const EditTaskTitle = (task: Task) => {
+        setEditTaskId(task.id);
+        setEditTask(task.task)
+    }
+    const saveTask = () =>{
+        if (editTask.trim() === '' || editTaskId === null) return;
+
+        setProblem(prev =>
+            prev.map(task =>
+                task.id == editTaskId ? {...task, task:editTask.trim()}: task
+            )
+        );
+        setEditTaskId(null)
+    };
+     const toggleComplete = (id: number) => {
+    setProblem(prev =>
+      prev.map(task =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
 
    
     return(
@@ -25,7 +72,43 @@ export const Main = () =>{
             <div className="list_con">
                 <div className="list">
                     <h1 className="list_title">Список дел</h1>
-                    {problems.map(problem =>(
+                     {problems.map(problem => (
+                        <div key={problem.id} className="list_task">
+                        <div className={`task_text ${problem.completed ? 'completed' : ''}`} onClick={() => toggleComplete(problem.id)}
+                            style={{ cursor: 'pointer' }}>
+                            {editTaskId === problem.id ? (
+                            <div className="edit-input-group">
+                                <input type="text" value={editTask} onChange={(e) => setEditTask(e.target.value)}
+                                onKeyDown={(e) => e.key === 'Enter' && saveTask()}/>
+                                <button onClick={saveTask}>Изменить</button>
+                            </div>
+                            ) : (
+                            problem.task
+                            )}
+                        </div>
+                        <div className="actions_icons">
+                            <img
+                            src="/edit (1).png"
+                            alt="Редактировать"
+                            className="change_task"
+                            onClick={(e) => {
+                                e.stopPropagation(); // не срабатывает toggleComplete
+                                EditTaskTitle(problem);
+                            }}
+                            />
+                            <img
+                            src="/delete (1).png"
+                            alt="Удалить"
+                            className="delete_task"
+                            onClick={(e) => {
+                                e.stopPropagation(); // не срабатывает toggleComplete
+                                DeleteTask(problem.id);
+                            }}
+                            />
+                        </div>
+                        </div>
+                    ))}
+                     {/*{problems.map(problem =>(
                         <div key={problem.id}>
                             <div className="list_task">
                                 <div className="task_text">{problem.task}</div>
@@ -36,97 +119,26 @@ export const Main = () =>{
                                 </div>
                             </div>
                         </div>
-                    ))}
+                    ))}*/}
+                  
 
                     <div className="new_task">
                         <h1>Новая запись</h1>
                         <div className="input_button">
-                            <input placeholder="Новая запись"></input>
-                            <button>Добавить</button>
+                            <input placeholder="Новая запись" value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}></input>
+                            <button onClick={addTask}>Добавить</button>
                         </div>
                     </div>
 
                 </div>
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
         </div>
+        </div>
+
+
+
+
 
     );
 }
